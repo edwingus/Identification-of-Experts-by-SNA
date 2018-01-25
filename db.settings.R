5fb247900418b9d438b3921d64c65e0aa8d84286
edwin71us <edwin71us@yahoo.com>
2018-01-25 13:12:42 -0800

    Update db.settings.R

--- db.settings.R
+++ db.settings.R
@@ -23,7 +23,7 @@ db.settings <- function(db, node.type, term.type = NA) {
                   'com' = {
                     # Columns to extract from the data for each node type selected
                     col <- switch(node.type,
-                                       actor = c("Author", "Author.affiliation", "Document.type", "DOI", "IPC.code", "Title"),
+                                       actor = c("Author", "Author.affiliation", "Author", "DOI", "ID", "Title"),
                                        term = switch(term.type, title = "Title", abstract = "Abstract", 
                                                      keyword = c("Controlled.Subject.terms", "Uncontrolled terms")),
                                        affl = "Author.affiliation",
